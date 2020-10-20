2639a29839ec776851274bbdd78e82b450101efd
Flávio José Formis <flavio.formis@pca.com.br>
2020-10-20 10:22:49 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DefaultInterfaceMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDefaultInterface i = new Implementacao();
+
+            Console.WriteLine(i.Nome());
+        }
+    }
+
+    interface IDefaultInterface
+    {        
+        string Nome(){
+            return "Meu nome padrão";
+        }
+    }
+
+    class Implementacao : IDefaultInterface
+    {
+        /*Descomentando o método abaixo, deixaremos de utilizar o método default da interface
+        e passaremos utilizar o método específico da classe."*/
+
+        /*public string Nome()
+        {
+            return "teste";
+        }*/
+    }
+    
+}
